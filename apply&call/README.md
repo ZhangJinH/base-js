55b8384d3ffca6caf466564215e839358aec82ff
张锦辉 <zhangjinhui@yazuo.com>
2019-05-14 17:18:23 +0800

    apply & call 了解

---
+++ apply&call/README.md
@@ -0,0 +1,9 @@
+# apply & call
+均可以修改this指向
+apply的参数为   arguments数组
+call的参数为    ...arguments (数组解构即可)
+
+## attention
+多重继承时
+如果继承的Object属性有冲突
+取最后一个继承的Object的值
---
+++ apply&call/apply.js
@@ -0,0 +1,70 @@
+function Human(sex) {
+  this.sex = sex;
+  this.conflict = `human`;
+  this.judgeSex = function () {
+    console.log(`i'm ${this.sex}, ${this.conflict}`);
+  }
+}
+
+/**
+ * 继承
+ * @param {String} name 男性名称
+ */
+function Man (name) {
+  Human.apply(this, [`man`]);
+  this.manName = name;
+  this.conflict = `man`;
+  this.manSay = function () {
+    this.judgeSex();
+    console.log(`man name: ${this.manName}, ${this.conflict}`);
+  }
+}
+
+/**
+ * 继承
+ * @param {String} name 女性名称
+ */
+function Woman(name) {
+  Human.apply(this, [`woman`]);
+  this.womanName = name;
+  this.conflict = `woman`;
+  this.womanSay = function () {
+    this.judgeSex();
+    console.log(`woman name: ${this.womanName}, ${this.conflict}`);
+  }
+}
+
+/**
+ * 多重集成
+ * 注意 key冲突 以最后一个Apply的Object为主
+ * @param {String} sex 性别
+ * @param {String} name 姓名
+ * @param {String} father 父亲名称
+ * @param {String} mother 母亲名称
+ */
+function Child(sex, name, father, mother) {
+  Human.apply(this, [sex]);
+  Woman.apply(this, [father]);
+  Man.apply(this, [mother]);
+
+  this.childName = name;
+
+  this.childSay = function () {
+    console.log(`
+      i'm: ${this.sex},
+      father: ${this.manName},
+      mother: ${this.womanName},
+      childName: ${this.childName},
+      ${this.conflict}
+    `);
+  }
+}
+
+let nick = new Man(`nick`);
+let elise = new Woman(`elise`);
+
+let john = new Child(`man`, `john`, `nick`, `elise`);
+
+nick.manSay();
+elise.womanSay();
+john.childSay();
